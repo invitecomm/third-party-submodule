ce6566a94cdbc3e1bb575312f1c437057beb328a
poing <brian.lavallee@invite-comm.jp>
2017-06-11 09:41:43 +0900

    Testing Moduel/ENV

---
+++ tests/config/modenv.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/* vim: set expandtab tabstop=4 shiftwidth=4 encoding=utf-8: */
+// +----------------------------------------------------------------------+
+// | MCAT Portal - Multi-Carrier Aggregate Trunking Client Portal         |
+// +----------------------------------------------------------------------+
+// |  © Copyright - INVITE Communications Co., Ltd. - All Rights Reserved |
+// |       Unauthorized copying of this file is strictly prohibited       |
+// |                     Proprietary and Confidential                     |
+// |                                                                      |
+// | 1F Computec Ichigaya Building                                        |
+// | 2-20 Ichigaya-Honmuracho                                             |
+// | Shinjuku-ku, Tokyo, Japan 162-0845                                   |
+// +----------------------------------------------------------------------+
+// | Contact: Brian LaVallee <brian.lavallee@invite-comm.jp>              |
+// +----------------------------------------------------------------------+
+//
+
+$config['path']	=	'config';
+$config['def']		=	TRUE;
+$config['defenv']		=	FALSE;
+$config['mod']		=	FALSE;
+$config['modenv']		=	FALSE;
